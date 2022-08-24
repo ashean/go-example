68c263adbb6cab143fd47489ada705364222f517
ashean <anthony.ashe+git@gmail.com>
2022-08-24 21:15:08 +0200

    Initial commit
    Changes to be committed:
    new file:   go.mod
    new file:   go.sum
    new file:   main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+		log.Info().Msg(r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
